5ac4674b664722b267bf29882ed3fff54c9493b8
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-01-22 22:10:07 -0300

    avancado

--- criptografia e autenticacao/app3/api/route.js
+++ criptografia e autenticacao/app3/api/route.js
@@ -29,22 +29,43 @@ server.route('/').get((req,res)=>{
 
     return db.insert(user)
              .table('authdata')
-             .then(_ => res.status(201).json( ))
+             .then(_ => res.status(201).json())
              .catch(err => res.status(400).send(err))
 })
 
 server.route('/:id').delete((req,res)=>{
+    const userId = { ...req.params }
 
+    return db.where({id: userId.id})
+            .delete()
+            .table('authdata')
+            .then(_ => res.status(204).json())
+            .catch(err => res.status(404).send(err))
 }).put((req,res)=>{
-    
-}).get((req,res)=>{
-     const user = {  ...req.params }
+    const user   = {  ...req.body  }
+    const userId = { ...req.params }
 
-      db.where({ id: user.id })
+    try{
+        method(user.girlname, 'Nome não informado!')
+        method(user.yearofmeet, 'Ano não informado!')
+        method(user.statusofloved, 'Status não informado!')
+    }catch(err){
+        return res.status(400).send(err)
+    }
+
+    return db.where({id: userId.id})
+             .update(user)
+             .table('authdata')
+             .then(_ => res.status(202).json())
+             .catch(err => res.status(404).send(err))
+}).get((req,res)=>{
+    const userId = { ...req.params }
+
+      db.where({ id: userId.id })
         .select(['girlname','yearofmeet','statusofloved'])
         .table('authdata')
         .then(data => res.status(200).json(data))
-        .catch(err => res.status(500).send(err))
+        .catch(err => res.status(404).send(err))
 })
 
 module.exports = server
