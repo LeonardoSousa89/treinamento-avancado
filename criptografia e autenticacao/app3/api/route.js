4dab1b891a916a4bff6f5fbb0900863c3dfe7045
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-01-23 21:20:11 -0300

    avancado

--- criptografia e autenticacao/app3/api/route.js
+++ criptografia e autenticacao/app3/api/route.js
@@ -1,6 +1,7 @@
 const db     = require('../db/db')
 const method = require('../methods/validation')
 
+const bcrypt = require('bcrypt')
 const express = require('express')
 const server = express.Router()
 
@@ -11,10 +12,46 @@ server.route('/test-communicate').get((req,res)=>{
       .catch(err => res.status(500).send(err))
 })
 
+server.route('/signup').post((req,res)=>{
+    const user = {  ...req.body  }
+
+    const cryptograph = password =>{
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password,salt)
+    }
+
+    try{
+        method(user.username, 'Nome de usuário não informado!')
+        method(user.email, 'Email não informado!')
+        method(user.pass, 'senha não informada!')
+    }catch(err){
+        return res.status(500).send(err)
+    }
+
+    user.pass = cryptograph(user.pass)
+
+    db.insert(user)
+      .table('authtest')
+      .then(_ => res.status(201).json())
+      .catch(err => res.status(400).send(err))
+})
+
+//estou aqui!
+// método gerando erros!
 server.route('/login').post((req,res)=>{
-    return res.json({
-        msg:'login area'
-    })
+
+    if(!req.body.email || !req.body.pass) return res.status(400).send('Informe usuário e senha!')
+
+    const searchedUser = db.table('authtest')
+                           .where({ email: req.body.email })
+                           .first()
+
+    if(!searchedUser) return res.status(400).send('Usuário não encontrado!')
+
+    const findUser = bcrypt.compareSync(req.body.pass, searchedUser.pass)
+    if(!findUser)  return res.status(404).send('Usuário e senha inválidos!')
+
+    return res.send(200).send('Usuário logado com sucesso!   ;)')
 })
 
 server.route('/girlfriend').get((req,res)=>{
